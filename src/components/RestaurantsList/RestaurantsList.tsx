ab8ae080e409b64c521439b3a9aaf44c5085f2dc
delenjer <delenjer@gmail.com>
2021-02-19 13:10:56 +0200

    add infinity scroll item restaurants

--- src/components/RestaurantsList/RestaurantsList.tsx
+++ src/components/RestaurantsList/RestaurantsList.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import { loadingRestaurants } from '../../store/thunk/thunk';
 import { getRestaurantsMemo } from '../../store/store';
@@ -10,19 +11,29 @@ import *as selectors from '../../store/store';
 import { Spinner } from "../Spinner/Spinner";
 
 export const RestaurantsList: React.FC = () => {
+  const [hasMore, setHasMore] = useState(true);
+  const dispatch = useDispatch();
   const restaurantsData = useSelector((state: IState) => getRestaurantsMemo(state));
   const restaurantsOptions = useSelector((state: IState) => selectors.getRestaurants(state));
   const isLoading = useSelector((state: IState) => selectors.isLoadingRestaurants(state));
-  const dispatch = useDispatch();
-
   const { initItems, addedItems } = restaurantsOptions;
   let currItems = initItems + addedItems;
   const restaurants = [...restaurantsData].slice(0, currItems);
-  let disabledButton = restaurantsData.length <= restaurants.length;
 
   useEffect(() => {
     dispatch(loadingRestaurants());
   }, []);
+
+  const fetchMoreData = () => {
+    if (restaurants.length >= restaurantsData.length) {
+      setHasMore(false);
+      return;
+    }
+
+    setTimeout(() => {
+      dispatch(setItems(restaurantsOptions.addedItems + 8));
+    }, 1000);
+  }
 
   return (
     <>
@@ -31,22 +42,27 @@ export const RestaurantsList: React.FC = () => {
           <Spinner />
         ) : (
           <div className="main-wrapper">
-            <section className="restaurant">
-              <ul className="restaurant__list">
-                {
-                  restaurants.map((restaurant: IRestaurants) => (
-                    <RestaurantsItem key={restaurant.uuid} restaurant={restaurant} />
-                  ))
-                }
-              </ul>
-            </section>
-
-            <button
-              disabled={disabledButton}
-              onClick={() => dispatch(setItems(restaurantsOptions.addedItems + 4))}
-              type="button">
-                Add restaurants
-            </button>
+            <InfiniteScroll
+              dataLength={restaurants.length}
+              next={fetchMoreData}
+              hasMore={hasMore}
+              loader={<div className="loader">Loading...</div>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              <section className="restaurant">
+                <ul className="restaurant__list">
+                  {
+                    restaurants.map((restaurant: IRestaurants) => (
+                      <RestaurantsItem key={restaurant.uuid} restaurant={restaurant} />
+                    ))
+                  }
+                </ul>
+              </section>
+            </InfiniteScroll>
           </div>
         )
       }
