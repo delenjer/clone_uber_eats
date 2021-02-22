d527718779a15beb2b61ea6cb2ca7279bc159e96
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2021-02-22 18:25:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.6b952defbdee3ea658b7686c34515646.js"
+  "/react_uber-eats/precache-manifest.41afb9ff7aaf09de5626361771dd12c7.js"
 );
 
 self.addEventListener('message', (event) => {
